7de5c79b5c0d9e8e777a0c607266a51657afddab
patipan-patisampita <patipan.patisampita@gmail.com>
2023-06-20 12:46:14 +0700

    firs commit

---
+++ bin/dart_week6.dart
@@ -0,0 +1,10 @@
+void main(){
+  int x =10; 
+  double y = 5;
+  print("Arithemetic x + y: $x + $y = ${x + y}");//1.Addition
+  print("Arithemetic x - y: ");//2.Subtraction
+  print("Arithemetic x * y: ");//3.Multiply
+  print("Arithemetic x / y: ");//4.Division
+  print("Arithemetic x ~/y: ");//5.Division returning integer
+  print("Arithemetic x % y: ");//6.Modulus
+}
